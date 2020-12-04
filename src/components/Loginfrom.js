0dc24dd248be42162092c2f24c4d8309031d86e5
Nontawit14042540 <6110210190@email.psu.ac.th>
2020-12-04 15:27:33 +0700

    41263.1530

--- src/components/Loginfrom.js
+++ src/components/Loginfrom.js
@@ -6,11 +6,15 @@ export default class Loginfrom extends Component{
 	render(){
 		return(
 			<React.Fragment>
-				<card className="card" id={styles.card}>
-					<div className="card-header">
+				<div className="card" id={styles.card}>
+					<div className="card-title my-3">
 						<h5>ระบบขอใช้รถ สำนักประชาสัมพันธ์เขต 6</h5>
 					</div>
-				</card>
+						<div className="card-img-top" id={styles.img}>
+							image
+						</div>
+
+				</div>
 		   </React.Fragment>
 		)
 	}
